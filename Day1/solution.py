e8ae105e4ca94f2150cea47ee9a2251dee8fd711
timeenjoyed <timeenjoyed.twitch@gmail.com>
2023-12-01 20:16:46 -0800

    added python type hints

--- Day1/solution.py
+++ Day1/solution.py
@@ -1,17 +1,18 @@
 import re
+import typing
 
 with open('input.txt') as file:
-  string_list = [line.strip() for line in file]
+  string_list: list[str] = [line.strip() for line in file]
 
 # calibration value can be found by combining first digit and LAST digit in that order, to form a single 
 # two-digit number.
 
-p1_total = 0
+p1_total: int = 0
 
-number_letters = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+number_letters: list[str] = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
 for line in string_list: 
-  line_num = ""
+  line_num: str = ""
   for i in line:
     if i.isdigit():
       line_num += i
@@ -30,15 +31,15 @@ with open('sample2.txt') as file:
   string_list2 = [line.strip() for line in file]
 
 regex = r'(?=(\d|one|two|three|four|five|six|seven|eight|nine))'
-digit_dict = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six":"6", "seven": "7", "eight": "8", "nine": "9"}
+digit_dict: dict[str, str] = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six":"6", "seven": "7", "eight": "8", "nine": "9"}
 
 
-p2_total = 0
+p2_total:int = 0
 
 for line in string_list:
   print(line)
-  line_num2 = ""
-  all_matches = re.findall(regex, line)
+  line_num2 :str = ""
+  all_matches: list[str] = re.findall(regex, line)
   print(all_matches)
   if all_matches[0].isdigit():
     line_num2 += all_matches[0]
@@ -49,6 +50,6 @@ for line in string_list:
   if not all_matches[-1].isdigit():
     line_num2 += digit_dict[all_matches[-1]]
   print(line_num2) 
-  int_num2 = int(line_num2)
+  int_num2: int = int(line_num2)
   p2_total += int_num2
 print(p2_total)
